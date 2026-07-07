7595b4a89d7fa42e3f182a979952274141de79c1
soroush <sorosh.hadi@gmail.com>
2026-07-07 12:51:31 +0200

    Added the einvironment files

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://hadiSolutions'
+    apiBaseUrl: 'api/',
+    hubPamentStatusUrl: 'hubs/payment-status',
+    stripePublicKey: 'pk_test_51TlPSNRcAV4Z6Sw3v5Z3lL7vaecIGBbbGbkHmLgkYTRlZp6JAkx8EbMpL4haBRP80PEah8YDiEz9qDFgXx65vSUa006LF3v3KV'
 }
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,7 @@
 
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://localhost:5057/api'
+    apiBaseUrl: 'http://localhost:5057/api/',
+    hubPamentStatusUrl: 'http://localhost:5057/hubs/payment-status',
+    stripePublicKey: 'pk_test_51TlPSNRcAV4Z6Sw3v5Z3lL7vaecIGBbbGbkHmLgkYTRlZp6JAkx8EbMpL4haBRP80PEah8YDiEz9qDFgXx65vSUa006LF3v3KV'
 }
